48cc014bd992e18432ad62d7aefd240c681744a5
registered-to-post-this <50346643+registered-to-post-this@users.noreply.github.com>
2019-06-16 05:14:02 -0700

    Update and rename addfwrs.bat to block-firewall.bat

---
+++ block-firewall.bat
@@ -0,0 +1,19 @@
+REM Adapted from https://github.com/charlesdh/addfwrs by Charles de Havilland
+@echo off
+cls
+set /p RULENAME="Type rule name: "
+ECHO Create in/out firewall rules to BLOCK *.exe files with the rulename of "%RULENAME%" ?
+ECHO.
+ECHO.
+
+pause
+Echo.
+FOR /r %%G in ("*.exe") Do (@echo %%G
+NETSH advfirewall firewall add rule name="%RULENAME%-%%~nxG" dir=in program="%%G" action="block" enable="yes")
+FOR /r %%G in ("*.exe") Do (@echo %%G
+NETSH advfirewall firewall add rule name="%RULENAME%-%%~nxG" dir=out program="%%G" action="block" enable="yes")
+Echo.
+Echo Done.
+Echo.
+Echo Batch complete. Press any key to exit.
+pause > NUL
